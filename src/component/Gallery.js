3477ca598bd415b43d0eb517f6e28be090ffbb36
Pankaj Patidar <pankaj_patidar@hotmail.com>
2023-08-30 18:53:29 +0530

    Added: Animations to load images.

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -1,18 +1,14 @@
-import React, {Component} from "react";
-import {Col, Container, Row} from "reactstrap";
-
-//import icon
+import React, { Component } from "react";
+import { Col, Container, Row } from "reactstrap";
 import FeatherIcon from "feather-icons-react";
-
-
-// feature Image
 import Img1 from "../assets/images/activities/a-1.jpeg";
 import Img2 from "../assets/images/activities/a-2.jpeg";
 import Img3 from "../assets/images/activities/a-3.jpeg";
 import Img4 from "../assets/images/activities/a-4.jpeg";
 import Img5 from "../assets/images/activities/a-5.jpeg";
+import "./style.css";
 
-export default class Project extends Component {
+export default class Gallery extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -51,29 +47,27 @@ export default class Project extends Component {
                                 <div className="text-center mb-5">
                                     <h2 className="">Spreading the Awareness</h2>
                                     <p className="text-muted">
-                                    "Kisano Ka Apna Bazar team is dedicated to bring in social awareness among the farmers and ensure visits to
-                                        villages regarding the products farmers use."
+                                        "Kisano Ka Apna Bazar team is dedicated to bringing social awareness among the farmers and ensuring visits to villages regarding the products farmers use."
                                     </p>
                                 </div>
                             </div>
                         </Row>
                         <Row>
-                            {/* Render Footer Link */}
                             {this.state.images.map((item, key) => (
                                 <Col lg={4} md={6} key={key}>
-                                    <div className="project-box mb-4">
+                                    <div className="project-box mb-4 gallery-box">
                                         <div className="position-relative overflow-hidden rounded">
-                                            <img src={item.img} alt="" className="img-fluid d-block mx-auto shadow"/>
+                                            <img
+                                                src={item.img}
+                                                alt=""
+                                                className="img-fluid d-block mx-auto shadow gallery-img"
+                                            />
                                             <div className="project-overlay">
                                                 <div className="project-content">
-                                                    <div className="project-icon" style={{
-                                                        cursor: "pointer"
-                                                    }}>
-                                                        <a href={item.img} style={{
-                                                            textDecoration: "none"
-                                                        }}>
+                                                    <div className="project-icon" style={{ cursor: "pointer" }}>
+                                                        <a href={item.img} style={{ textDecoration: "none" }}>
                                                             <i>
-                                                                <FeatherIcon icon="eye"/>
+                                                                <FeatherIcon icon="zoom-in" size={40} />
                                                             </i>
                                                         </a>
                                                     </div>
