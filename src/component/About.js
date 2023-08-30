a4ba4fbc13da3da5bf0f98caa63f7d54bc0cfe65
Pankaj Patidar <pankaj_patidar@hotmail.com>
2023-08-30 12:40:18 +0530

    Updated comments

--- src/component/About.js
+++ src/component/About.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { Col, Container, Row } from "reactstrap";
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
+import {Col, Container, Row} from "reactstrap";
 
 // Import Background Image
 import Img2 from "../assets/images/about/img-2.png";
@@ -24,36 +24,36 @@ export default class About extends Component {
                         </Row>
                         <Row>
                             <Col lg={6} md={12}>
-                <span className="font-weight-medium">
-                  <h4 className="line-height-1_6">
-                    किसानों का अपना बाजार शाजापुर से स्टार्ट हुआ एक संस्थान
-                    है जो किसानों को खाद बीज दवाई एग्री एक्सपर्ट के माध्यम से
-                    घर तक पहुंचाने का काम करता है इन प्रोडक्ट का समय पर
-                    उपयोग कर किसान अपनी फसलों का उत्पादन बढ़ाकर अच्छी उपज ले
-                    साथ ही गुणवत्ता बढ़ाकर अपनी फसलों का अच्छा भाव ले सके
-                    सके इन सभी सुविधाओं को किसानों तक पहुंचाने के लिए किसानों
-                    का अपना बाजार ने एक पहल कि हे अपनी ब्रांच बढ़ाकर वह
-                    ज्यादा से ज्यादा किसानों तक पहुंच सके यह शाखाएं अभी तक 1
-                    शाजापुर 2 बेरछा 3 बोड़ा 4 सुजालपुर में पहुंच चुकी है 1
-                    शाजापुर में टंकी चौराहे के पास है 2 बेरछा में यहां sbi
-                    bank के पास है 3 वोडा में यहां सोसाइटी के सामने नरसिंह
-                    रोड पर है
-                  </h4>
-                </span>
-                                <br />
-                                <br />
-                                <br />
+                                <span className="font-weight-light">
+                                  <p className="line-height-1_6">
+                                    किसानों का अपना बाजार शाजापुर से स्टार्ट हुआ एक संस्थान
+                                    है जो किसानों को खाद बीज दवाई एग्री एक्सपर्ट के माध्यम से
+                                    घर तक पहुंचाने का काम करता है इन प्रोडक्ट का समय पर
+                                    उपयोग कर किसान अपनी फसलों का उत्पादन बढ़ाकर अच्छी उपज ले
+                                    साथ ही गुणवत्ता बढ़ाकर अपनी फसलों का अच्छा भाव ले सके
+                                    सके इन सभी सुविधाओं को किसानों तक पहुंचाने के लिए किसानों
+                                    का अपना बाजार ने एक पहल कि हे अपनी ब्रांच बढ़ाकर वह
+                                    ज्यादा से ज्यादा किसानों तक पहुंच सके यह शाखाएं अभी तक 1
+                                    शाजापुर 2 बेरछा 3 बोड़ा 4 सुजालपुर में पहुंच चुकी है 1
+                                    शाजापुर में टंकी चौराहे के पास है 2 बेरछा में यहां sbi
+                                    bank के पास है 3 वोडा में यहां सोसाइटी के सामने नरसिंह
+                                    रोड पर है |
+                                  </p>
+                                </span>
+                                <br/>
+                                <br/>
+                                <br/>
 
                                 <p className="f-17 text-muted">
-                                    <FeatherIcon className="icon-xs icon mr-2" icon="grid" />{" "}
+                                    <FeatherIcon className="icon-xs icon mr-2" icon="grid"/>{" "}
                                     Empowering Farmers
                                 </p>
                                 <p className="f-17 text-muted">
-                                    <FeatherIcon className="icon-xs icon mr-2" icon="layout" />{" "}
+                                    <FeatherIcon className="icon-xs icon mr-2" icon="layout"/>{" "}
                                     Quality and Value
                                 </p>
                                 <p className="f-17 text-muted mb-4">
-                                    <FeatherIcon className="icon-xs icon mr-2" icon="monitor" />{" "}
+                                    <FeatherIcon className="icon-xs icon mr-2" icon="monitor"/>{" "}
                                     Expert Assistance
                                 </p>
                                 <Link to="#" className="read-more f-18">
